c8386b7ec6b575595165d804590837788b75ccd2
Wolfjawan <mohsen000069@gmail.com>
2019-03-24 11:33:10 +0000

    service worker unregisterd

--- src/components/aboutUs/contactDetails.js
+++ src/components/aboutUs/contactDetails.js
@@ -9,9 +9,17 @@ class ContactDetails extends Component {
           <h2>Contact Details</h2>
           <p>simple text</p>
           <ul>
-            <li>Email: something</li>
+            <li>Email: teresa.byrne@bdc.ac.uk</li>
             <li>Tell: 01287463453435</li>
-            <li>Address: something </li>
+            <li>
+              Address:{" "}
+              <a
+                target="_blanc"
+                href="https://www.google.co.uk/maps/place/Eastbrookend+Country+Park+Millennium+Visitor+Centre/@51.5528668,0.1747286,17.68z/data=!4m5!3m4!1s0x0:0xfbd0965e9b74b82c!8m2!3d51.5527702!4d0.1760743?hl=en&authuser=0"
+              >
+                The Chase, Dagenham, Romford RM7 0SS
+              </a>
+            </li>
           </ul>
         </div>
         <div className="content-details">
