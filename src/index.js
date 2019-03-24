2063477afa2fca1854d9c481cf219d7703856d5d
Wolfjawan <mohsen000069@gmail.com>
2019-03-24 11:26:27 +0000

    service worker unregisterd

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 ReactDOM.render(<App />, document.getElementById('root'));
-serviceWorker.register();
+serviceWorker.unregister();
